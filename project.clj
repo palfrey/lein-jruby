3d92ea7d3fa61dc67ad0a61b62925d679fae9bdf
Tom Parker <palfrey@tevp.net>
2016-06-17 14:58:10 +0100

    Upgrade JRuby to 1.7

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lshift/lein-jruby "0.1.2-SNAPSHOT"
   :description "Run JRuby commands from Leiningen."
-  :dependencies [[org.jruby/jruby-complete "1.6.7"]
+  :dependencies [[org.jruby/jruby-complete "1.7.25"]
                  [lancet "1.0.1"]]
   :eval-in-leiningen true)
